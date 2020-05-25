f362c83671e404bcf7624ec51b2ae9d4395eecb0
pvrmza <pablo@pampa.cloud>
2020-05-25 13:58:57 -0300

    fix wget quiet

--- scripts/EVE-NG/sync-labsvault.sh
+++ scripts/EVE-NG/sync-labsvault.sh
@@ -24,7 +24,7 @@ else
 fi
 
 # auto update !!! 
-wget https://raw.githubusercontent.com/pvrmza/labsvault/master/scripts/EVE-NG/sync-labsvault.sh -O /tmp/sync-labsvault.sh
+wget -q https://raw.githubusercontent.com/pvrmza/labsvault/master/scripts/EVE-NG/sync-labsvault.sh -O /tmp/sync-labsvault.sh
 if [ $? -eq 0 ]; then
 	cp /tmp/sync-labsvault.sh /etc/cron.hourly/sync-labsvault.sh
 	chmod 755 /etc/cron.hourly/sync-labsvault.sh
