986790133c33a560cd851901b1b135345f92b7e6
pvrmza <pablo@pampa.cloud>
2020-05-25 20:49:47 -0300

    fix fix fix

--- scripts/EVE-NG/sync-labsvault.sh
+++ scripts/EVE-NG/sync-labsvault.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-logger "iniciando..."
+LOG="/var/log/sync-labsvault.log"
+
+echo "iniciando..." > $LOG
+date >> $LOG
 #
 DIRLOCALLABS=/opt/unetlab/labs/labsvault
 DIRREMOTELABS=EVE-NG
@@ -9,24 +12,26 @@ URLLABS=https://github.com/pvrmza/labsvault/trunk/$DIRREMOTELABS/
 # svn -> tmp
 cd /tmp
 rm -rf $DIRREMOTELABS
-svn checkout $URLLABS 
+svn checkout $URLLABS >> $LOG
 if [ $? -eq 0 ]; then
-	# tmp -> dir local
-	rm -rf $$DIRLOCALLABS && mkdir -p $DIRLOCALLABS
-	rsync -avz --exclude='.svn*' --delete $DIRREMOTELABS/* $DIRLOCALLABS
+        # tmp -> dir local
+        rm -rf $$DIRLOCALLABS && mkdir -p $DIRLOCALLABS
+        rsync -avz --exclude='.svn*' --delete $DIRREMOTELABS/* $DIRLOCALLABS >> $LOG
 
-	# fix 
-	chown www-data.www-data $DIRLOCALLABS -R 
-	find $DIRLOCALLABS -type f -exec chmod 555 {} \; 
-	logger "lab sync [OK]"
+        # fix 
+        chown www-data.www-data $DIRLOCALLABS -R
+        find $DIRLOCALLABS -type f -exec chmod 555 {} \;
+        echo "lab sync [OK]" >> $LOG
 else
-	logger "fallo SVN CHECKOUT"
+        echo "fallo SVN CHECKOUT" >> $LOG
 fi
 
 # auto update !!! 
 wget -q https://raw.githubusercontent.com/pvrmza/labsvault/master/scripts/EVE-NG/sync-labsvault.sh -O /tmp/sync-labsvault.sh
 if [ $? -eq 0 ]; then
-	cp /tmp/sync-labsvault.sh /etc/cron.hourly/sync-labsvault.sh
-	chmod 755 /etc/cron.hourly/sync-labsvault.sh
-	logger "auto update [OK]"
+        cp /tmp/sync-labsvault.sh /etc/cron.hourly/sync-labsvault.sh
+        chmod 755 /etc/cron.hourly/sync-labsvault.sh
+        echo "auto update [OK]" >> $LOG
+else
+        echo "fallo descarga autoupdate" >> $LOG
 fi
--- scripts/EVE-NG/update_images.sh
+++ scripts/EVE-NG/update_images.sh
@@ -1,48 +1,51 @@
 #!/bin/bash
 
+LOG="/var/log/update_images.log"
 BASE="/opt/unetlab/addons"
 LIST="images.list"
+
+date > $LOG
 
 # download new images.list
 rm -rf ${BASE}/${LIST}
 megaurl=`curl -s https://kutt.it/lv-images-list | cut -b 23-`
-megadl --path=${BASE} $megaurl
+megadl --path=${BASE} $megaurl >>$LOG
 
 if ! test -f ${BASE}/${LIST} ; then
-	# if download fail... exit...
-	echo "${BASE}/${LIST} not exist"
-	exit 1
+        # if download fail... exit...
+        echo "${BASE}/${LIST} not exist" >> $LOG
+        exit 1
 fi
 
 
 while IFS= read -r line
-do   
-	DIR=`echo $line | awk -F\| '{ print $1 }'`
-	FILE=`echo $line | awk -F\| '{ print $2 }'`
-	SHA1=`echo $line | awk -F\| '{ print $3 }'`
-	TINYURL=`echo $line | awk -F\| '{ print $4 }'`
-	URL=`curl -s $TINYURL | cut -b 23-`
+do
+        DIR=`echo $line | awk -F\| '{ print $1 }'`
+        FILE=`echo $line | awk -F\| '{ print $2 }'`
+        SHA1=`echo $line | awk -F\| '{ print $3 }'`
+        TINYURL=`echo $line | awk -F\| '{ print $4 }'`
+        URL=`curl -s $TINYURL | cut -b 23-`
 
-	FileLocal=${BASE}${DIR}${FILE}
+        FileLocal=${BASE}${DIR}${FILE}
 
-	if test -d ${BASE}${DIR} ; then
-		if test -f ${FileLocal} ; then
-			sha1=`sha1sum ${FileLocal} | awk '{ print $1}' `
-			if [ $SHA1 != $sha1 ]; then
-				rm -rf ${FileLocal}
-				echo "Downloading ${FILE} in ${BASE}${DIR}..."
-				mkdir -p ${BASE}${DIR} && megadl --path=${FileLocal} ${URL}
-			else
-				echo "${FileLocal} is update"
-			fi
-		else
-			echo "Downloading ${FILE} in ${BASE}${DIR}..."
-			mkdir -p ${BASE}${DIR} && megadl --path=${FileLocal} ${URL}
-		fi
-	else
-		echo "Downloading ${FILE} in ${BASE}${DIR}..."
-		mkdir -p ${BASE}${DIR} && megadl --path=${FileLocal} ${URL}
-	fi
-done < <( cat ${BASE}/${LIST} )
+        if test -d ${BASE}${DIR} ; then
+                if test -f ${FileLocal} ; then
+                        sha1=`sha1sum ${FileLocal} | awk '{ print $1}' `
+                        if [ $SHA1 != $sha1 ]; then
+                                rm -rf ${FileLocal}
+                                echo "Downloading ${FILE} in ${BASE}${DIR}..." >> $LOG
+                                mkdir -p ${BASE}${DIR} && megadl --path=${FileLocal} ${URL} >> $LOG
+                        else
+                                echo "${FileLocal} is update" >> $LOG
+                        fi
+                else
+                        echo "Downloading ${FILE} in ${BASE}${DIR}..." >> $LOG
+                        mkdir -p ${BASE}${DIR} && megadl --path=${FileLocal} ${URL} >> $LOG
+                fi
+        else
+                echo "Downloading ${FILE} in ${BASE}${DIR}..." >> $LOG
+                mkdir -p ${BASE}${DIR} && megadl --path=${FileLocal} ${URL} >> $LOG
+        fi
+done < <( cat ${BASE}/${LIST} ) >> $LOG
 
-/opt/unetlab/wrappers/unl_wrapper -a fixpermissions
+/opt/unetlab/wrappers/unl_wrapper -a fixpermissions >> $LOG
