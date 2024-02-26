4c0bec84d755341c4b07aa1b435c0938101eef65
Pablo Vargas <pvargas@telecentro.net.ar>
2024-02-26 18:56:04 -0300

    Primer commit del 2024

--- scripts/eve-ng_tunning.sh
+++ scripts/eve-ng_tunning.sh
@@ -5,10 +5,14 @@ while fuser /var/lib/dpkg/lock-frontend; do
     sleep 5
 done
 
-apt-get update && apt-get -y dist-upgrade
-apt-get install -y qemu-guest-agent open-vm-tools subversion rsync build-essential \
+#apt-get update && apt-get -y dist-upgrade
+#apt-get install -y qemu-guest-agent open-vm-tools subversion rsync build-essential \
+  # libglib2.0-dev libssl-dev libcurl4-openssl-dev libgirepository1.0-dev pkg-config \
+  #genisoimage cloud-utils screen byobu 
+
+apt-get update && apt-get install -y qemu-guest-agent open-vm-tools subversion rsync build-essential \
   libglib2.0-dev libssl-dev libcurl4-openssl-dev libgirepository1.0-dev pkg-config \
-  genisoimage cloud-utils screen byobu 
+  genisoimage cloud-utils screen byobu   
 if [ $? -ne 0 ]; then
 	exit 1
 fi
